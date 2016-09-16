7cae101da118cc93ceb0e01ecd0e141b302c7c42
Marc Harper <marc.harper@gmail.com>
2016-09-15 23:13:34 -0700

    Graph Moran yen search examples

--- yen_search.py
+++ yen_search.py
@@ -1,13 +1,4 @@
 
-"""
-Yen Search Algorithm:
-- From a given initial state
-    - compute the yen to each neighboring state
-    - move in the direction of max or min (depending on extrema desired)
-    - stop if all positive (negative)
-- Improvement: use Boltzmann distribution to break ties and escape local extrema
-"""
-#
 # # Standard Libraries
 # from collections import defaultdict
 # from itertools import izip, islice
@@ -19,19 +10,33 @@ Yen Search Algorithm:
 # import matplotlib.gridspec as gridspec
 # import pandas
 
+import copy
+import random
+
 import numpy as np
 
 # # Github libraries from https://github.com/marcharper
 # import mpsim
 # import stationary
 # from stationary.processes import incentive_process
-# from stationary.processes.incentives import replicator, fermi, linear_fitness_landscape
+from stationary.processes.incentives import replicator, fermi, linear_fitness_landscape
 # from stationary.utils.bomze import bomze_matrices
 # import ternary
 
 
 def yen_search(initial_state, neighbor_function, transition_function,
                beta=None, extrema="max"):
+    """
+    Yen Search Algorithm:
+    - From a given initial state
+        - compute the yen to each neighboring state
+        - move in the direction of max or min (depending on extrema desired)
+        - stop if all positive (negative)
+    - Improvement: use Boltzmann distribution to break ties and escape local extrema
+    """
+
+    visited_states = []
+
     state = initial_state
     while True:
         print(state)
@@ -50,8 +55,14 @@ def yen_search(initial_state, neighbor_function, transition_function,
             if all(y < 0 for y in yens.values()):
                 return state
         # Move to next state
-        next_state = min(yens.keys(), key=(lambda key: yens[key]))
+        visited_states.append(state)
+        min_value = min(yens.values())
+        min_keys = [k for k in yens.keys() if yens[k] == min_value]
+        next_state = random.choice(min_keys)
+        # next_state = min(yens.keys(), key=(lambda key: yens[key]))
         state = next_state
+        if state in visited_states:
+            return "Repeated State"
 
 def two_type_test():
     """Test the yen search algorithm on the neutral fitness landscape for a
@@ -87,5 +98,98 @@ def two_type_test():
     e = yen_search(state, neighbor_function, transition_function)
     print(e)
 
+
+def graph_test():
+    """Test yen search on large process -- a two type neutral Moran process on
+    a cycle."""
+    def neighbor_function(state):
+        """State in this case is a list of ones and zeroes."""
+        neighbors = []
+        for i, t in enumerate(state):
+            neighbor = copy.copy(list(state))
+            if t == 0:
+                neighbor[i] = 1
+            else:
+                neighbor[i] = 0
+            neighbors.append(tuple(neighbor))
+        return neighbors
+
+    # print(neighbor_function([0, 1, 0]))
+
+    def transition_function(source, target, mu=0.05):
+        # Find the state that differs
+        for i, (s, t) in enumerate(zip(source, target)):
+            if s != t:
+                break
+        # i is now the index of the state that differs
+        # Look at neighboring states (within the cycle) to determine transition
+        N = len(source)
+        indices = [i-1, i+1]
+        transition = 0.
+        for index in indices:
+            if source[index % N] == s:
+                transition += 1 - mu
+            else:
+                transition += mu
+        return transition
+
+    state = [0, 1, 0, 1, 0, 0]
+    e = yen_search(state, neighbor_function, transition_function)
+    print(e)
+
+    state = [0, 1, 0, 1, 1, 1]
+    e = yen_search(state, neighbor_function, transition_function)
+    print(e)
+
+    # state = [0, 0, 0, 1, 1, 1]
+    # e = yen_search(state, neighbor_function, transition_function)
+    # print(e)
+
+    # Non-neutral landscape test
+
+    m = [[1,2], [2,1]] # Hawk-Dove
+    fitness_landscape = linear_fitness_landscape(m)
+    incentive = replicator(fitness_landscape)
+
+    def transition_function2(source, target, mu=0.01):
+        """Non-neutral landscape."""
+        # Find the state that differs
+        for i, (s, t) in enumerate(zip(source, target)):
+            if s != t:
+                break
+        # i is now the index of the state that differs
+        s = sum(source)
+        N = len(source)
+        population_state = (N - s, s)
+        inc = incentive(population_state)
+        denom = float(sum(inc))
+        indices = [i-1, i+1]
+        transition = 0.
+        for index in indices:
+            rep_type = source[index % N]
+            r = float(inc[rep_type]) / denom
+            if rep_type == s:
+                transition += r * (1 - mu)
+            else:
+                transition += r * mu
+        return transition
+
+    state = [0, 1, 0, 1, 0, 1, 0, 1, 1, 0]
+    e = yen_search(state, neighbor_function, transition_function2)
+    print(e)
+
+    state = [0, 1, 1, 0, 1, 1, 0, 0, 0, 1]
+    e = yen_search(state, neighbor_function, transition_function2)
+    print(e)
+
+    state = [0, 1] * 8
+    e = yen_search(state, neighbor_function, transition_function2)
+    print(e)
+
+    state = [0, 1] * 16
+    e = yen_search(state, neighbor_function, transition_function2)
+    print(e)
+
 if __name__ == "__main__":
-    two_type_test()
+    # two_type_test()
+    graph_test()
